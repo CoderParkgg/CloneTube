5fc41adb862b2c9cd74465e9b2c864420dfea82c
CoderParkgg <67491015+CoderParkgg@users.noreply.github.com>
2021-07-21 15:08:58 +0900

    middleware!

--- src/server.js
+++ src/server.js
@@ -1,2 +1,43 @@
-import express from "express";  //node_modules/express 라고 할 필요 없이 이렇게 사용하면 됨.
-//후자 : package. 전자 : express라는 이름으로 불러온다.
+import express from "express";
+
+
+const PORT = 3001;
+const app = express();
+
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    //console.log(req.url);
+    if (url === "/protected") {
+        console.log("Not allowed");
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed.");
+    next();
+}
+
+const handleHome = (req, res) => {
+    return res.send("Hello :)");
+};
+const handleLogin = (req, res) => {
+    return res.send({ login: "hi" });
+};
+const handleProtected = (req, res) => {
+    return res.send("<h1>Welcome to the private lounge</h1>");
+};
+
+
+app.use(logger, privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
+
+
+
+const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`)
+app.listen(PORT, handleListening); //request를 듣늗다. callback함수를 사용하여 일정 port에 요청이 오면 callback함수 실행
