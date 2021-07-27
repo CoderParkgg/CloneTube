a7173ad1b1edd422f9ff1c0f3a90414c05358a7b
CoderParkgg <67491015+CoderParkgg@users.noreply.github.com>
2021-07-27 11:47:44 +0900

    use pug

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,10 +1,10 @@
 
 //Home
-export const trending = (req, res) => res.send(`Home Page Video`);
+export const trending = (req, res) => res.render("home", { pageTitle : "Home" });
 export const search = (req, res) => res.send("Search");
 
 //video routers
-export const edit = (req, res) => res.send(`Edit`);
-export const see = (req, res) => res.send("Watch");
+export const edit = (req, res) => res.render("edit", {pageTitle : "edit"});
+export const see = (req, res) => res.render("watch", {pageTitle : "watch"});
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
