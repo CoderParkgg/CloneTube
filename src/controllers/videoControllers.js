6f00d643cf61f8bd68df0d5bd37db58e90b99585
CoderParkgg <67491015+CoderParkgg@users.noreply.github.com>
2021-08-12 17:25:28 +0900

    step

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,15 +1,18 @@
-import Video from "../models/Video"; //mhdel 수입하기.
+import Video from "../models/Video"; //model 수입하기.
 
 //Home
 
 export const home = async(req, res) => {
-    const videos = await Video.find({})
-    console.log(videos);
-    return res.render("home", { pageTitle : "Home", videos: []})
+   try {
+       const videos = await Video.find({})
+       return res.render("home", { pageTitle : "Home", videos})
+    }catch{
+    }
 };
 export const search = (req, res) => res.send("Search");
 
 //video routers
+
 export const getEdit = (req, res) =>{
     const { id } = req.params;
     return res.render("edit", {pageTitle : `Editing :`});
@@ -28,17 +31,28 @@ export const deleteVideo = (req, res) => res.send("Delete Video");
 export const getUpload = (req, res) => {    
     return res.render("upload", {pageTitle : "Upload Video"});
 };
-export const postUpload = (req, res) => {
-    const {title, description, hashtag} = req.body;
-    const video = new Video({ //model에는 타입만 지정했지만, 여기서는 직접 값을 넣어준다. 
+export const postUpload = async (req, res) => {
+    const {title, description, hashtags} = req.body;
+    // const video = new Video({ //model에는 타입만 지정했지만, 여기서는 직접 값을 넣어준다. 
+    //     title, //title : title 과 같음. 여기서 후자는 위의 title의 값을 뜻함.
+    //     description,
+    //     createdAt : Date.now(), //Date.now는 1970년 이래로 밀리세컨드 초로 제공됨.
+    //     hashtags:hashtags.split(",").map(word => `#${word}`),
+    //     meta:{
+    //         views:0,
+    //         rating:0,
+    //     }
+    // });
+    // await video.save();
+    await Video.create({ //model에는 타입만 지정했지만, 여기서는 직접 값을 넣어준다. 
         title, //title : title 과 같음. 여기서 후자는 위의 title의 값을 뜻함.
         description,
-        createdAt : Date.now(),
+        createdAt : Date.now(), //Date.now는 1970년 이래로 밀리세컨드 초로 제공됨.
+        hashtags:hashtags.split(",").map(word => `#${word}`),
         meta:{
             views:0,
             rating:0,
         }
     });
-
     return res.redirect("/");
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({  //형식만 정해줌
   title: String,  //내용을 넣지 않고 형식만 
   description: String, //이는 {type: String} 과 같다. 그냥 짧게 쓸려고 그런것
-  createdAt: Date,
+  createdAt: {type : Date, require : true},
   hashtags: [{type: String}], //배열인 경우 [], 그 안의 형식을 정할 경우 [{}]
-  meta: {
+  meta: {  //사용자에게 자동으로 제공하는 것들
       views:Number,
       rating:Number,
   },
